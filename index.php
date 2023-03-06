c6406f681c3b494f271eb929890f70ad85923f7b
Mizikova Yuliya <galashina@bk.ru>
2023-03-06 12:14:42 +0300

    workshop2

--- index.php
+++ index.php
@@ -9,8 +9,26 @@
 <body>
 <?php
 
-require 'src/ws1.php';
+//require 'src/ws1.php';
 require 'src/functions.php';
+
+
+echo task1(['Привет', ',', 'Вася', '!', 'Как дела?'], true);
+echo task1(['Привет', ',', 'Вася', '!', 'Как дела?']);
+echo task2('/', 1, 2, 3, 5.2);
+echo task3(5, 3);
+echo Task4();
+echo '<br>';
+echo task5('24.02.2016 00:00:00');
+echo '<br>';
+echo task6('К', ' ', 'Карл у Клары украл Кораллы');
+echo '<br>';
+echo task6('Две', 'Три', 'Две бутылки лимонада');
+echo '<br>';
+echo task7('text.txt', 'Hello again!');
+echo '<br>';
+echo task8('text.txt');
+
 
 ?>
 </body>
